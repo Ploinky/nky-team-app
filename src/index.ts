bcf9230a2c9968009d078ef68be3be41950b5ceb
Ploinky <mail@ploinky.de>
2025-12-24 14:05:20 +0100

    build detail pages

--- src/index.ts
+++ src/index.ts
@@ -6,14 +6,22 @@ import {serveStatic} from 'hono/bun'
 import { PlayerStatsPage } from './pages/player-stats.page'
 import { MatchesPage } from './pages/matches.page'
 import { YearInOinkyPage } from './pages/year-in-oinky.page'
+import { ssgParams } from 'hono/ssg'
+import { spring2025Split } from '../data/2025spring/split'
+import { fall2025Split } from '../data/2025fall/split'
+import { oinky_players } from '../data/oinky-players'
 
 const app = new Hono()
 
 app.get('/', IndexPage)
 app.get('/players', PlayerStatsPage)
-app.get('/players/:puuid', PlayerDetailPage)
+app.get('/players/:puuid',
+    ssgParams(() => oinky_players.map(p => ({puuid: p.puuid}))),
+    PlayerDetailPage)
 app.get('/matches', MatchesPage)
-app.get('/matches/:matchId', MatchDetailPage)
+app.get('/matches/:matchId',
+    ssgParams(() =>  [...spring2025Split.matches, ...fall2025Split.matches].map(m => ({matchId: m.matchData.metadata.matchId}))),
+    MatchDetailPage)
 app.get('/yearinoinky', YearInOinkyPage)
 
 app.use('/static/*', async (c, next) => {
